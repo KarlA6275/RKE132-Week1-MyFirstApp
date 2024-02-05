74cee5395146cecc380a5235ded1e7cdbc67b3cd
KarlA <Karl.adamson@tktk.ee>
2024-02-06 00:38:51 +0200

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿//This is a comment to my code
+
+//Rakendus küsib kasutajalt sisestada tema nimi
+
+//Rakendus tervitab kasutajat nimepidi
+Console.WriteLine("Enter your name:"); //Output
+
+//String - Sõne
+String UserName = Console.ReadLine(); //Input
+
+Console.WriteLine("Hello" + ", " + UserName + "!"); //Output
+
+//string interpolation
+Console.WriteLine($"Hello, {UserName} !"); //Output
